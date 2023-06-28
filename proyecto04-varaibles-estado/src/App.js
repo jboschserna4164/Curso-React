66beda9a35a72059ccbd9fdeda5603ccc2c42dfa
jboschserna4164 <62222010+jboschserna4164@users.noreply.github.com>
2023-06-28 17:54:32 -0500

    generando 5 numeros aleatorios y aprendiendo sobre el useState

--- proyecto04-varaibles-estado/src/App.js
+++ proyecto04-varaibles-estado/src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  function generarAleatorio() {
-    const enteroAleatorio = Math.trunc(Math.random() * 10); /* se genera un aleatorio entre 0 y 9 */
-    setNumero(enteroAleatorio);
+  const [numeros, setNumeros] = useState([0,0,0,0,0]);
+
+  function generarAleatorios() {
+    const vector = new Array(5);
+    for (let i = 0; i < vector.length; i++) {
+      vector[i] = Math.trunc(Math.random() * 10);
+    }
+    setNumeros(vector);
   }
   
-  const [numero, setNumero] = useState(0);
-
   return(
   <div>
-    <p>Numero aleatorio: {numero}</p>
-    <button onClick={generarAleatorio}>Generar numero aleatorio</button>
+    <p>Numero aleatorios: </p>
+    {numeros.map(num => (<p>{num}</p>))}  {/* Para mostrar todos los elemento del vector llamamos al método map de la clase Array y le pasamos una función flecha que retorna para cada elemento del vector un valor encerrado entre las marcas 'p'*/}
+    <button onClick={generarAleatorios}>Generar numeros aleatorios</button>
   </div>
   );
 }
